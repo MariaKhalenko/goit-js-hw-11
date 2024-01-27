b608fe5c27bda49de9ffc2a465267ae5f87d1a6d
MariaKhalenko <134129926+MariaKhalenko@users.noreply.github.com>
2024-01-27 18:08:40 +0000

    Deploying to gh-pages from @ MariaKhalenko/goit-js-hw-11@675c190c2e1d84eb36d1c181f6fb0f1d2bd5836a 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,24 +1,24 @@
-import{i as l,S as y}from"./assets/vendor-46aac873.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const c=document.querySelector(".form-search"),p=document.querySelector(".search-box"),u=document.querySelector(".gallery"),a=document.querySelector(".loader"),f="https://pixabay.com/api",h="23963114-6d0d5d874ae460d9125bacd21";a.style.display="none";c.addEventListener("submit",function(n){n.preventDefault();const r=encodeURIComponent(p.value.trim());if(r.trim()===""){l.error({title:"Error",message:"Nothing found"});return}function i(e){if(u.innerHTML="",a.style.display="block",a.style.display="none",e.length===0){l.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}),a.style.display="none";return}const t=m(e);u.innerHTML=t,new y(".gallery a",{captionsData:"alt",captions:!0,captionDelay:250}).refresh(),c.reset()}const s=`${f}/?key=${h}&q=${r}&image_type=photo&orientation=horizontal&safesearch=true`;fetch(s).then(e=>e.json()).then(e=>{i(e.hits)}).catch(e=>{console.error(e)})});function m(n){return n.map(({webformatURL:r,largeImageURL:i,tags:s,likes:e,views:t,comments:o,downloads:d})=>`<li class="gallery-item">
+import{i as l,S as y}from"./assets/vendor-46aac873.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))o(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const s of e.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&o(s)}).observe(document,{childList:!0,subtree:!0});function i(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerpolicy&&(e.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?e.credentials="include":t.crossorigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function o(t){if(t.ep)return;t.ep=!0;const e=i(t);fetch(t.href,e)}})();const c=document.querySelector(".form-search"),p=document.querySelector(".search-box"),u=document.querySelector(".gallery"),a=document.querySelector(".loader"),f="https://pixabay.com/api",m="23963114-6d0d5d874ae460d9125bacd21";a.style.display="none";c.addEventListener("submit",function(n){n.preventDefault();const r=encodeURIComponent(p.value.trim());if(r.trim()===""){l.error({title:"Error",message:"Enter search data"});return}const i=new y(".gallery a",{captionsData:"alt",captions:!0,captionDelay:250});function o(e){if(u.innerHTML="",a.style.display="block",e.length===0){l.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}),a.style.display="none";return}const s=h(e);u.innerHTML=s,i.refresh()}c.reset();const t=`${f}/?key=${m}&q=${r}&image_type=photo&orientation=horizontal&safesearch=true`;fetch(t).then(e=>e.json()).then(e=>{o(e.hits)}).catch(e=>{console.error(e)})});function h(n){return n.map(({webformatURL:r,largeImageURL:i,tags:o,likes:t,views:e,comments:s,downloads:d})=>`<li class="gallery-item">
           <a class="gallery-link" href="${i}">
             <img
               class="gallery-image"
               src="${r}"
-              alt="${s}"
+              alt="${o}"
               width="360"
             />
           </a>
           <div class="info-section">
             <div class="section">
               <h2 class="tittle">Likes</h2>
-              <p class="quantity">${e}</p>
-            </div>
-            <div class="section">
-              <h2 class="tittle">Views</h2>
               <p class="quantity">${t}</p>
             </div>
             <div class="section">
+              <h2 class="tittle">Views</h2>
+              <p class="quantity">${e}</p>
+            </div>
+            <div class="section">
               <h2 class="tittle">Comments</h2>
-              <p class="quantity">${o}</p>
+              <p class="quantity">${s}</p>
             </div>
             <div class="section">
               <h2 class="tittle">Downloads</h2>
